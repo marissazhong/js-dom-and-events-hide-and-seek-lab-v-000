eeac42425d52dfdc2b175b02c562879da8bc62d3
Marissa Zhong <mwz2101@columbia.edu>
2018-07-19 18:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const lis = document.querySelectorAll('ul.ranked-list li');
   for (let i=0; i<lis.length; i++) {
-    lis[i].innerHTML += n;
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
   }
 }
 
